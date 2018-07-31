b27e4e9c0aa9f5feda7225d9fcfdc48c439db701
Sebastián Estrella <sestrella.me@gmail.com>
2018-07-31 11:58:56 -0500

    Change test script

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,6 +6,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_nvm(host):
-    r = host.check_output('/root/.nvm/versions/node/v8.11.3/bin/node -v')
-    assert r == "v8.11.3"
+def test_node_version(host):
+    v = host.check_output('/root/.nvm/versions/node/v8.11.3/bin/node -v')
+    assert v == "v8.11.3"
