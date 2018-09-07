29314374923b690ac25fb597469e197b94ccf30e
Sebastián Estrella <sestrella.me@gmail.com>
2018-09-06 23:08:21 -0500

    Fix linter warnings

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,6 +6,9 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_node_version(host):
-    v = host.check_output('/root/.nvm/versions/node/v8.11.3/bin/node -v')
-    assert v == "v8.11.3"
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
