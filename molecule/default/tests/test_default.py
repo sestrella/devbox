8ee3686dd1e0bbcec0a5099b34d62855b2e9662f
Sebastián Estrella <sestrella.me@gmail.com>
2018-10-11 00:22:43 -0500

    Remove some platforms

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,13 +6,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_nodejs(host):
-    cmd = host.run('/root/.asdf/installs/nodejs/8.12.0/bin/node --version')
-
-    assert cmd.rc == 0
-    assert '8.12.0' in cmd.stdout
-
-
 def test_python(host):
     cmd = host.run('/root/.asdf/installs/python/3.7.0/bin/python --version')
 
