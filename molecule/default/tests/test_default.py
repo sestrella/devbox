f9e2e3179f6c8677d3c7986c1449602ff87706a4
Sebastián Estrella <sestrella.me@gmail.com>
2018-10-11 00:17:57 -0500

    Refactor tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,22 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_nodejs(host):
+    cmd = host.run('/root/.asdf/installs/nodejs/8.12.0/bin/node --version')
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert cmd.rc == 0
+    assert '8.12.0' in cmd.stdout
+
+
+def test_python(host):
+    cmd = host.run('/root/.asdf/installs/python/3.7.0/bin/python --version')
+
+    assert cmd.rc == 0
+    assert '3.7.0' in cmd.stdout
+
+
+def test_ruby(host):
+    cmd = host.run('/root/.asdf/installs/ruby/2.5.1/bin/ruby --version')
+
+    assert cmd.rc == 0
+    assert '2.5.1' in cmd.stdout
