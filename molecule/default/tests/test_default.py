a43cd47e20022368f3af15a641075794ead9ce50
Sebastián Estrella <sestrella.me@gmail.com>
2019-01-01 23:48:43 -0500

    Add pyenv tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,6 +6,12 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+def test_pyenv_version(host):
+    cmd = host.run('/root/.pyenv/bin/pyenv --version')
+
+    assert cmd.rc == 0
+    assert '1.2.8' in cmd.stdout
+
 def test_rbenv_version(host):
     cmd = host.run('/root/.rbenv/bin/rbenv --version')
 
