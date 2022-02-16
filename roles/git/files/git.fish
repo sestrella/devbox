c714a73376aa55b02e588fb661d0001d1f24d98f
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2022-02-16 02:02:34 -0500

    Major refactoring

---
+++ roles/git/files/git.fish
@@ -0,0 +1,7 @@
+abbr ga git add
+abbr gaa git add --all
+abbr gc git commit -v
+abbr gc! git commit -v --amend
+abbr gco git checkout
+abbr gd git diff
+abbr gst git status
--- roles/neovim/files/init.lua
+++ roles/neovim/files/init.lua
@@ -1,1 +1,3 @@
+require('plugins')
+
 vim.o.number = true
