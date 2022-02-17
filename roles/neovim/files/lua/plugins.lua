2da146e73d98f8d379002dabeabb93107d81d72c
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2022-02-16 22:01:32 -0500

    Config telescope and tree

--- roles/neovim/files/lua/plugins.lua
+++ roles/neovim/files/lua/plugins.lua
@@ -1,4 +1,4 @@
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
 return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
@@ -7,12 +7,17 @@ return require('packer').startup(function()
     'kyazdani42/nvim-tree.lua',
     requires = { 'kyazdani42/nvim-web-devicons' },
     config = function()
-      require'nvim-tree'.setup {}
+      require('nvim-tree').setup()
+      vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<cr>', {})
     end
   }
 
   use {
     'nvim-telescope/telescope.nvim',
-    requires = { 'nvim-lua/plenary.nvim' }
+    requires = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('telescope').setup()
+      vim.api.nvim_set_keymap('n', '<C-p>', '<cmd>Telescope find_files<cr>', {})
+    end
   }
 end)
