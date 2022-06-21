2aaab09c3c2038ecf39ec80865f2e80e12beffdd
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2022-06-21 15:45:30 -0500

    Add packer_compiled.lua file

--- roles/neovim/files/config/lua/plugins.lua
+++ roles/neovim/files/config/lua/plugins.lua
@@ -110,24 +110,7 @@ return require('packer').startup(function()
     requires = {
       'hrsh7th/cmp-vsnip',
       'hrsh7th/vim-vsnip'
-    },
-    config = function()
-      local cmp = require('cmp')
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body)
-          end
-        },
-        mapping = {
-          ['<Tab>'] = cmp.mapping.confirm({ select = true })
-        },
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'vsnip' }
-        })
-      })
-    end
+    }
   }
 
   use {
