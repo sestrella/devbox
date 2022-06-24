5dd564408a1712c7cb41cd069e455c10fee70221
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2022-06-24 13:55:46 -0500

    Remove lua_format

--- roles/neovim/files/config/after/plugin/vim-lua-format.lua
+++
@@ -1,7 +0,0 @@
-local group = vim.api.nvim_create_augroup("lua_format", {})
-
-vim.api.nvim_create_autocmd({"BufWrite"}, {
-  pattern = "*.lua",
-  callback = function() vim.cmd([[call LuaFormat()]]) end,
-  group = group
-})
