270858e1cbeb903b99f1e99bfdc821414c435aee
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2022-06-24 14:07:14 -0500

    Delegate packer installation to neovim

--- roles/neovim/files/config/lua/plugins.lua
+++ roles/neovim/files/config/lua/plugins.lua
@@ -9,7 +9,11 @@ vim.api.nvim_create_autocmd({"BufWritePost"}, {
   group = group
 })
 
-vim.cmd([[packadd packer.nvim]])
+local packer_bootstrap = false
+local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  packer_bootstrap = vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
 
 return require("packer").startup(function(use)
   use "wbthomason/packer.nvim"
@@ -47,4 +51,8 @@ return require("packer").startup(function(use)
       vim.cmd("colorscheme solarized")
     end
   }
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
